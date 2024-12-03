f02eb4332e675c4daffd29989ed30c1db149cdb7
H4ZM0G <160122665+H4ZM0G@users.noreply.github.com>
2024-12-03 15:08:13 +0100

    Migration done + slidebar a jour

--- app/Views/templates/admin/sidebar.php
+++ app/Views/templates/admin/sidebar.php
@@ -1,13 +1,14 @@
 <div class="sidebar sidebar-dark sidebar-fixed border-end" id="sidebar">
     <div class="sidebar-header border-bottom">
         <div class="sidebar-brand">
-            <a href="<?= base_url('/admin'); ?>"><img src="<?= base_url('/assets/brand/logo-blanc.svg'); ?>" class="sidebar-brand-full" _width="88"
+<!--            gere le logo        -->
+            <a href="<?= base_url('/admin'); ?>"><img src="<?= base_url('/assets/brand/PPTournamentSLAM.svg'); ?>" class="sidebar-brand-full" _width="88"
             height="32"
             alt="Mon Projet" /></a>
-            <a href="<?= base_url('/admin'); ?>"><img src="<?= base_url('/assets/brand/logo-blanc.svg'); ?>" class="sidebar-brand-narrow" _width="32"
+            <a href="<?= base_url('/admin'); ?>"><img src="<?= base_url('/assets/brand/PPTournamentSLAM.svg'); ?>" class="sidebar-brand-narrow" _width="32"
                  height="32"
                  alt="Mon Projet" /></a>
-            Mon Projet
+              SLAM
         </div>
         <a href="<?= base_url(); ?>" target="_blank" class="header-toggler" alt="Voir le site" title="Voir le site"><i class="fa-solid fa-house-laptop"></i></a>
     </div>
