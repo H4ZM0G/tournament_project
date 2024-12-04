d2e117f7bdcac03bdfe1e81da83cbba44be6001e
H4ZM0G <160122665+H4ZM0G@users.noreply.github.com>
2024-12-04 17:41:06 +0100

    Update SchoolModel.php

--- app/Models/SchoolModel.php
+++ app/Models/SchoolModel.php
@@ -6,58 +6,119 @@ use CodeIgniter\Model;
 
 class SchoolModel extends Model
 {
-    protected $table            = 'school';
-    protected $primaryKey       = 'id';
+    protected $table = 'school';
+    protected $primaryKey = 'id';
 
     // Champs permis pour les opérations d'insertion et de mise à jour
-    protected $protectFields    = true;
-    protected $allowedFields    = ['name', 'city', 'score', 'created_at', 'updated_at', 'deleted_at'];
+    protected $allowedFields = ['name', 'city', 'score','created_at', 'updated_at', 'deleted_at'];
 
-    // Dates
-    protected $useTimestamps = false;
+    // Activer le soft delete
+    protected $useSoftDeletes = true;
+
+    // Champs de gestion des dates
+    protected $useTimestamps = true;
     protected $createdField  = 'created_at';
     protected $updatedField  = 'updated_at';
     protected $deletedField  = 'deleted_at';
 
     // Validation
-    protected $validationRules      = [
-        'name' => 'required|min_length[0]|max_length[100]',
-        'city' => 'required|min_length[0]|max_length[100]',
+    protected $validationRules = [
+        'name' => 'required|is_unique[school.name,id,{id}]|min_length[3]|max_length[100]',
+        'city' => 'required|min_length[3]|max_length[100]',
     ];
 
-    protected $validationMessages   = [
+    protected $validationMessages = [
         'name' => [
-            'required'   => 'Le nom de l\'établissement est requis.',
-            'min_length' => 'Le nom de l\'établissement doit comporter au moins 0 caractères.',
-            'max_length' => 'Le nom de l\'établissement ne doit pas dépasser 100 caractères.',
-
+            'required'   => 'Le nom d\'ecole est requis.',
+            'min_length' => 'Le nom d\'ecole doit comporter au moins 3 caractères.',
+            'max_length' => 'Le nom d\'ecole ne doit pas dépasser 100 caractères.',
+            'is_unique'   => 'Ce nom d\'ecole est déja utilisé.',
         ],
         'city' => [
             'required'   => 'Le nom de la ville est requis.',
-            'min_length' => 'Le nom de la ville doit comporter au moins 0 caractères.',
+            'min_length' => 'Le nom de la ville doit comporter au moins 3 caractères.',
             'max_length' => 'Le nom de la ville ne doit pas dépasser 100 caractères.',
-
         ],
     ];
-    public function getAllSchool() {
-        $builder = $this->db->table('school s');
 
-        $builder->select("s.id, s.name, s.city, s.score, s.created_at, s.updated_at, s.deleted_at");
+    // Callbacks pour le hachage du mot de passe
+    protected $beforeInsert = ['hashPassword'];
+    protected $beforeUpdate = ['hashPassword'];
 
-        $builder->orderBy('s.id', 'ASC');
-        return $builder->get()->getResultArray();
+    protected function hashPassword(array $data)
+    {
+        if (!isset($data['data']['password'])) {
+            return $data;
+        }
+
+        $data['data']['password'] = password_hash($data['data']['password'], PASSWORD_DEFAULT);
+        return $data;
     }
 
-    public function getschoolById($id) {
+    // Relations avec les permissions
+    public function getPermissions()
+    {
+        return $this->join('user_permission', 'user.id_permission = user_permission.id')
+            ->select('user.*, user_permission.name as permission_name')
+            ->findAll();
+    }
+
+    public function getSchoolById($id)
+    {
         return $this->find($id);
     }
+    public function getAllSchools()
+    {
+        return $this->findAll();
+    }
 
-    public function deleteSchool($id) {
+    public function createSchool($data)
+    {
+        return $this->insert($data);
+    }
+    public function updateSchool($id, $data)
+    {
+        return $this->update($data);
+    }
+    public function deleteSchool($id)
+    {
         return $this->delete($id);
     }
 
-    public function updateSchool($id, $data) {
-        return $this->update($id, $data);
+    public function getPaginatedSchool($start, $length, $searchValue, $orderColumnName, $orderDirection)
+    {
+        $builder = $this->table('school');
+        // Recherche
+        if ($searchValue != null) {
+            $builder->like('name', $searchValue);
+            $builder->orLike('city', $searchValue);
+        }
+
+        // Tri
+        if ($orderColumnName && $orderDirection) {
+            $builder->orderBy($orderColumnName, $orderDirection);
+        }
+
+        $builder->limit($length, $start);
+
+        return $builder->get()->getResultArray();
     }
 
+    public function getTotalUser()
+    {
+        $builder = $this->table('school');
+        return $builder->countAllResults();
+    }
+
+    public function getFilteredUser($searchValue)
+    {
+        $builder = $this->table('school');
+        // @phpstan-ignore-next-line
+        if (! empty($searchValue)) {
+            $builder->like('name', $searchValue);
+            $builder->orLike('city', $searchValue);
+        }
+
+        return $builder->countAllResults();
+    }
 }
