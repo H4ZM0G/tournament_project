2daebb209071be7733e02e747497b9f6849a3013
H4ZM0G <160122665+H4ZM0G@users.noreply.github.com>
2024-12-22 22:37:02 +0100

    Update index.php

--- app/Views/admin/school/index.php
+++ app/Views/admin/school/index.php
@@ -23,11 +23,11 @@
                     <td><?= htmlspecialchars($school['city']); ?></td>
                     <td><?= htmlspecialchars($school['category_name']); ?></td>
                     <td>
-                        <a href="<?= base_url("admin/school/edit/{$school['id']}"); ?>"><i class="fa-solid fa-pencil"></i></a>
+                        <a href="<?= base_url('/admin/school/edit/'. $school['id']); ?>"><i class="fa-solid fa-pencil"></i></a>
                     </td>
                     <td>
                         <?= ($school['deleted_at'] === null) ?
-                            "<a title='Désactiver lécole href='" . base_url("admin/school/deactivate/{$school['id']}") . "'><i class='fa-solid fa-xl fa-toggle-on text-success'></i></a>" :
+                            "<a title='Désactiver lécole' href='" . base_url("admin/school/deactivate/{$school['id']}") . "'><i class='fa-solid fa-xl fa-toggle-on text-success'></i></a>" :
                             "<a title='Activer lécole' href='" . base_url("admin/school/activate/{$school['id']}") . "'><i class='fa-solid fa-toggle-off fa-xl text-danger'></i></a>";
                         ?>
                     </td>
@@ -37,40 +37,3 @@
         </table>
     </div>
 </div>
-<script>
-    $(document).ready(function () {
-        var baseUrl = "<?= base_url(); ?>";
-        var dataTable = $('#gameTable').DataTable({
-            "responsive": true,
-            "processing": true,
-            "serverSide": true,
-            "ajax": {
-                "url": baseUrl + "admin/game/SearchGame", // Assurez-vous que cette route est correcte
-                "type": "POST"
-            },
-            "columns": [
-                {"data": "id"},
-                {"data": "name"},
-                {
-                    "data": "id",
-                    "sortable": false,
-                    "render": function(data) {
-                        return `<a href="${baseUrl}admin/game/edit/${data}"><i class="fa-solid fa-pencil"></i></a>`;
-                    }
-                },
-                {
-                    "data": "id",
-                    "sortable": false,
-                    "render": function(data, type, row) {
-                        return (row.deleted_at === null ?
-                            `<a title="Désactiver le jeu" href="${baseUrl}admin/game/deactivate/${data}"><i class="fa-solid fa-xl fa-toggle-on text-success"></i></a>` :
-                            `<a title="Activer le jeu" href="${baseUrl}admin/game/activate/${data}"><i class="fa-solid fa-toggle-off fa-xl text-danger"></i></a>`);
-                    }
-                }
-            ],
-            "language": {
-                "url": baseUrl + 'js/datatable/datatable-2.1.4-fr-FR.json',
-            }
-        });
-    });
-</script>
